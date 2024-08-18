b1fe7147c8bb57a7e90592a59e0d248e6a5f5fcc
sualeh-farooq <sualehfarooq65@gmail.com>
2024-08-18 19:34:35 +0500

    Class Code of 17-18 August 2024
    Topics Covered
    CSS Completion
    Bootstrap Revsion
    Start of Javascript
    
    Javascript Chapters 1 - 4 Covered
    
    Alert
    Variable for String
    Variable for Numbers
    Variable Name Legal Illegal

---
+++ JS/18-8-2024/app.js
@@ -0,0 +1,39 @@
+// alert("Congratulations !\n\n\n\n\n\n\n You have passed the CSS Quiz")
+
+
+// window.alert('SAYLANI\nSAYLAN\nSAYLA\nSAYL')
+
+
+// String == "Sualeh"
+// Number / Integer === 2 , 3 235,
+// Boolean === TRUE OR FALSE
+
+
+
+
+
+// alert('My name is ' + myName + " \n My Nationality is "  + nationality)
+
+// alert('My name is Sualeh\nMy Nationality is Pakistani')
+
+// Single Line Comment
+
+
+var num1  ;
+num1 = 5
+var num2 = 5
+var num3 = '10'
+
+
+var myFirstName = 'sualeh'  // camel case
+var my_first_name = 'sualeh'  // snak case
+var myName;  // decalre
+myName = 'sualeh'; // defined
+
+var nationality = 'Pakistani'
+
+document.write('My name is ' + myFirstName + " And Nationality is " + nationality)  // this print data in your browser main window
+
+
+
+
