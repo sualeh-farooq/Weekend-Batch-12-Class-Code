a80c7bb48700d1ee2cf8e364af5fb82c04312a20
sualeh-farooq <sualehfarooq65@gmail.com>
2024-08-31 19:33:52 +0500

    Class Code  of 25-8 & 31-8-2024
    Topics Covered -->
    
    Bootstrap Carousel Revision
    Javascript Introduction
    First 4 Chapters of Javascript
    
    1- Alert
    2 - Variable for String
    3 - Variable for Numbers
    4 - Variable Name Legal and Illegal

---
+++ JS/31-8-2024/index.js
@@ -0,0 +1,61 @@
+// ************ ALERTS **********************
+
+
+// alert(false)
+// alert('String data here')
+
+// Data Types
+
+// "Sualeh@1233412212@##%^*" - String
+// 123456780.5582465   - Number / Integer
+// True - False       - Boolean
+
+
+
+
+// ************ VARAIBLE FOR STRING **********************
+
+// var myName ;
+// myName = 'Sualeh'
+// var nationality = "Pakistani";
+// alert(myName)
+// alert('My Nationality is Pakistani'  )
+// alert('My Nationality is ' + nationality )  STRING CONCETTINATING
+
+
+// ************ VARAIBLE FOR NUMBER **********************
+
+
+
+
+var num1 = "A15"
+var num2 = 10
+
+var result = num1 * num2
+
+alert(result)
+
+
+
+// 15 + 15 = "30"
+// "15" + 15 = "1515"
+// "50" - 10 = "40"
+// "15" + "10" = "1510"
+// "AB1" + 10 = "NaN"
+// "AB3" + '10' = "AB310"
+// "AB3" - '10' = "NaN"
+
+
+var rose = "first";
+alert(rose)
+
+rose = 2
+
+alert(rose)
+var Rose = "Second"
+
+
+function getMyName() {
+    console.log('Sualeh')
+}
+
