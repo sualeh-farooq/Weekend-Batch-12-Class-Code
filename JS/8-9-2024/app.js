0f7d6c236eacce574076fab4a83c4a33df7247e8
sualeh-farooq <sualehfarooq65@gmail.com>
2024-09-08 19:17:10 +0500

    Class Code of 8-9-2024
    Topics Covered =>
    
    Maths Familiar Operator
    Maths Unfamiliar Operators
    Concatenating Text Strings
    Prompts

---
+++ JS/8-9-2024/app.js
@@ -0,0 +1,39 @@
+// var a = 5
+// var b = 10
+
+
+// console.log(--b + a - ++a + b++)
+
+
+
+// var num1 = 2
+// var num2 = 2
+// var num3 = 10
+
+// var result = (10 + 50) * 10 + (50 - 4)
+
+
+
+// --num == pre decrement
+// num-- == post decrement
+// ++num == pre increment
+// num++ == post increment
+
+
+// var name = 'sualeh'
+// var secondName = 'farooq'
+
+// console.log(name +  " "  + secondName)
+
+
+
+
+
+var num1 = +prompt('Please Enter First Number')
+var num2 = +prompt('Please Enter Second Number')
+
+
+document.write('The Multiply of ' + num1 + " and " + num2 + " is " + Number(num1*num2) + "<br>" ) // br tag in string use for line break 
+document.write('The Multiply of ' + num1 + " and " + num2 + " is " + Number(num1*num2) )
+
+
