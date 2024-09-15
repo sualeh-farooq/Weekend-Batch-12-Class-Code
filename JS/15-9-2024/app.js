a1b189b29117950f2e3026ba791315c92e3a78c5
sualeh-farooq <sualehfarooq65@gmail.com>
2024-09-15 19:54:13 +0500

    Class Code of 15-9-2024
    Topics Covered =>
    Unfamiliar Operator
    Prompts
    If Else
    Else if
    Comparison Operators

---
+++ JS/15-9-2024/app.js
@@ -0,0 +1,148 @@
+// var a = 2
+// var b = 3
+
+
+// var result = --a + ++b + b++ + --b
+//         1 + 4 + 4 + 4
+
+
+// var totalCost = (1 + 3) * 4;
+
+// alert(totalCost)
+
+
+// var userName = 'John'
+
+// document.write("<h1>" + "My Name is " + userName + "</h1>")
+
+
+// var rollNo = 123
+// var percentage = 65
+
+// document.write(` 
+//     <table>
+//             <tr>
+//                     <th>Name </th>
+//                     <th>Roll No </th>
+//                     <th> Percentage </th>
+//             </tr>
+
+//             <tr>
+//                     <th> ${userName} </th>
+//                     <th> ${rollNo} </th>
+//                     <th> ${percentage} % </th>
+//             </tr>
+//     </table>
+//     `)
+
+
+// var userName = prompt("Enter your Name" , 'Ali')
+
+
+// var userNumber = +prompt('Enter Number for Multiplication' , 5)
+// alert(userNumber + userNumber)
+// document.write(`<p>  ${userNumber} x 1 = ${userNumber*1}`)
+
+
+
+// var num2 = 2
+// var num3 = 2
+
+// alert(num2 === num3)
+
+
+// var num2 = 10
+// var num3 = "10"
+
+
+
+// alert(typeof(num3))
+
+
+
+// var city = 'KaRaChI'
+// var city2 = 'karachi'
+
+
+// alert(city !== city2)
+
+// var number2 = 10
+// alert(number2 <= 10)
+
+// var userCity = prompt('Enter your city name')
+// if(userCity === 'karachi'){
+//     alert('You belong to the City of Light')
+// } else {
+//     alert('You belong to just a normal city')
+// }
+
+
+
+// if(userCity === 'karachi') {
+//     alert('Biryani')
+// } else if (userCity === 'islamabad') {
+//     alert('pulao')
+// } else if(userCity === 'quetta') {
+//     alert('Chai')
+// } else if(userCity === 'lahore') {
+//     alert('paye')
+// } else  {
+//      alert('samose')
+// }
+
+
+
+// var userAge = +prompt('Please Enter your Age')
+// if(userAge <= 15) {
+//     alert('You are just a kid')
+// } else if (userAge <= 22) {
+//     alert('You are a teenager')
+// } else if (userAge <= 30) {
+//     alert('You are an adult')
+// } else if(userAge <= 50) {
+//     alert('You are closed to Retired')
+// } else if (userAge <= 55) {
+//     alert('You are retired')
+// } else {
+//     alert('ALLAH ALLAH kro')
+// }
+
+
+var num = 0.25
+
+alert(num < 0.50)
+
+
+// var num4 = 4
+
+// var userVal = +prompt('enter number')
+
+
+
+// if(userVal === num4 ) {
+     
+// } else if(userVal  === ++num4)
+
+
+
+
+var percentage = 83
+
+
+var grade ;
+var remarks ;
+
+
+if(percentage >= 80) {
+    grade = "A+"
+    remarks= "Excellent"
+} else if (percentage >= 70) {
+      grade = "A+"
+    remarks= "Excellent"
+}
+
+document.write(`
+    
+    The Grade is ${grade}
+    <br />
+    `)
