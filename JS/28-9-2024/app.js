8c8324b710e6f94984caa7f1f90bc37ac064f859
sualeh-farooq <sualehfarooq65@gmail.com>
2024-09-28 19:41:56 +0500

    Class Code of 28-9-2024
    Topics Covered =>
    Arrays
    Arrays Methods =>
    
    Push,
    Pop,
    Unshift
    Shift ,
    Slice ,  ( Also work on string )
    Splice ,
    join ,
    reverse,
    split ,

---
+++ JS/28-9-2024/app.js
@@ -0,0 +1,119 @@
+// var person = 'sualeh'
+
+
+// var arr = ['ali' , 'john' , 'sualeh' , 'anas']
+        
+
+// var students = [] // length is 0
+
+// students[students.length] = 'Ali'   //students[0] = 'ali' => length is 1 students = ['Ali']
+// students[students.length] = 'Anas'   // students[1] = 'Anas'
+
+
+// console.log(students)
+// console.log(students.length)
+
+
+// var pets = ['Cat' , 'Dog' , 'Fish' , 'Goat']
+// var returnType = pets.pop() // return the deleted value
+
+// console.log(returnType)
+// console.log(pets)
+
+
+// var pets = ['Cat' , 'Dog'   ] // 2
+// var returnVal = pets.push('Goat' ) // 3 
+// console.log(returnVal)
+
+
+// var students = ['Ali' , 'Anas' , 'John']
+
+// students.shift()
+
+// console.log(students)
+
+// students.unshift('Shahmeer')
+
+// console.log(students)
+
+
+//delete any element from any index
+// update any element from any index
+// add any element at any index
+
+
+
+// var students = ['Ali' , 'Anas' , 'Amir' , 'Iqram' ,'john' , 'mike' , 'sarim' , 'rehan']
+
+
+
+// // students.splice(2 , 0 , 'usama') // Not Delete Any Element and Add Usama at Second Index
+// students.splice(4,3 , 'sualeh'  )  // Delete 3 Values from That Index and Update Value on that Index
+
+
+// var eliminatedStudents = students.slice(5)
+
+
+// console.log(eliminatedStudents)
+// console.log(students)
+
+
+
+var colors = ['Red' , 'Green' , 'Yellow' , 'Blue' , 'Purple' , 'Pink' , 'Black']
+
+
+// var desiredIndex = +prompt('Please Tell Your Desired Index to Add Value')
+
+// var desiredColor = prompt('Please name the color you wanna add')
+
+
+// colors.splice(desiredIndex , 0 , desiredColor )
+
+// document.write(colors)
+// var desiredIndex = +prompt('Please Tell Your Desired Index to Target')
+// var count = +prompt('How many values you wanna delete from that index')
+// colors.splice(desiredIndex , count)
+// document.write(colors)
+// colors.splice(4 , 0 , 'White' , 'Aqua')
+
+// console.log(colors)
+
+
+// var sentence = 'An Apples a Day Keeps Doctor Away'
+// var appleVal = sentence.slice(3,15)
+// console.log(appleVal)
+
+
+
+// var word = ['This' , 'Is' , 'My' , 'Cat']
+
+// var Updated = word.join(' ')  // how to join your arr into string
+// document.write(Updated)
+
+
+// var colors  = ['Red' , 'Yellow' , 'Green' ]
+
+// var reverse = colors.reverse()
+
+// console.log(reverse)
+
+
+
+// var students  = [] // JS Literal Notation
+
+// var colors = new Array // Object Notation 
+
+// console.log(colors)
+// console.log(students)
+
+
+
+// var colors = ['red' , 'yellow']
+
+// var a = colors.push('black')
+
+// console.log(a)
+
+
+var word = 'Deed'
+word.split('').reverse().join('')
