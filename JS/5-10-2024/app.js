6e929cc05e597bf5cc1b9044cb5d7e0a49779ccf
sualeh-farooq <sualehfarooq65@gmail.com>
2024-10-05 19:19:00 +0500

    Class code of 5th October 2024
    Topics Covered =>
    For Loop
    For Loop Nested

---
+++ JS/5-10-2024/app.js
@@ -0,0 +1,57 @@
+// For Loops
+
+// // 3 < 5 ===> TRUE => 3
+// // 4 < 5 ===> TRUE => 4
+// // 5 < 5 ===> FALSE
+// for(var i = 0; i <= 5; i++) {
+//     console.log(i)
+// }
+
+// var cities = ["karachi", "lahore", "peshawar", "multan"];
+// // var foundIt = false
+// var milGya = 'nahi'
+// for (var i = 0; i < cities.length; i++) {
+// //   if (cities[i] === "peshawar") {
+// //     console.log("Found it");
+// //     break;
+// //   } else {
+// //     console.log("Not Found");
+// //   }
+
+//     // if(cities[i] === 'quetta') {
+//     //    milGya = 'han'
+//     // }
+// }
+
+// if(milGya === 'han') {
+//     console.log('Found It')
+// } else {
+//     console.log('Not Found !')
+
+// }
+
+// For Loops Nested
+
+
+
+// var arr = [
+//     ['sualeh' , 'farooq'] ,
+//     ['ali' , 'ahmed'] ,
+//     ['zain' , 'mustafa']
+// ]
+// for(var i =0 ; i < arr.length; i++) {
+//         for(var j = 0; j < arr[i].length; j++) {
+//             console.log(arr[i][j])
+//         }
+// }
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+
+// for (var i = 0; i < firstNames.length; i++) {
+//   for (var j = 0; j < lastNames.length; j++) {
+//     fullNames.push(firstNames[i] + lastNames[j]);
+//   }
+// }
+// console.log(fullNames)
