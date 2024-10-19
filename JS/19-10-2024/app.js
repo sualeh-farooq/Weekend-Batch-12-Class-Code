dc3c04e2050de06a7c755e5cbad58020e7c1efde
sualeh-farooq <sualehfarooq65@gmail.com>
2024-10-19 19:31:46 +0500

    Class Code of 19-10-2024
    Topics Covered =>
    
    Revision of String Methods , Ch: 20-25
    
    And Math Methods
    
    Math.round
    Math.round
    Math.floor
    Math.ceil
    parseInt
    parseFloat

---
+++ JS/19-10-2024/app.js
@@ -0,0 +1,65 @@
+// var userName = prompt('Please enter your name').toUpperCase()
+
+
+// // AdMiN  , ADMIN , aDMIN ,  ==> admin  => ADMIN 
+// if(userName === 'admin') {
+//         window.location.href = 'profile.html'    
+// }
+
+// var sentence = 'An Apple a dAY KEEP DOCTOR AWAY'
+// var doctorWord = sentence.slice(0)
+// alert(doctorWord)
+
+
+
+// var sentence = 'An Apple a dAY KEEP DOCTOR AWAY Apple'
+// var indexOfApple = sentence.lastIndexOf('Saylani')
+// alert(indexOfApple)
+
+
+
+// Apple -> Mango
+// var sentence = 'An Apple a dAY KEEP DOCTOR AWAY Apple'
+// var updSentence  = sentence.replaceAll('Apple' , 'Mango') // replace all the occurence
+// var updSentence  = sentence.replace('Apple' , 'Mango')    ///just replace the first occurence
+// console.log(updSentence)
+
+// var num1 = 2.2
+// alert(Math.ceil(num1))  // 2
+
+
+
+
+// var randomNum  = Math.round(Math.random() * 6)
+
+// var randomNum  = Math.random() * 6
+// console.log(randomNum.toFixed(3))
+
+
+
+// Will Ignore All the Decimal Places 
+// var num1 = '5.6'
+// console.log(typeof(num1))
+// console.log(num1)
+
+
+// var num2 = parseInt(num1)
+// console.log(typeof(num2))
+// console.log(num2)
+
+// Will Keep All the Decimal Places 
+// var num1 = '5.6'
+// console.log(typeof(num1))
+// console.log(num1)
+
+// var num2 = parseFloat(num1)
+// console.log(typeof(num2))
+// console.log(num2)
+
+
+
+
+
+function GreetUser() {
+    alert('Hello User')
+}
