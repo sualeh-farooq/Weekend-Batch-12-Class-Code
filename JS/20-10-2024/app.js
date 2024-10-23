9f0b94106bb9fc7240bb477fa0e119edd71e0b14
sualeh-farooq <sualehfarooq65@gmail.com>
2024-10-23 05:53:13 -0700

    Class Code of 20-10-2024
    Topics Covered =>
    Math methods
    Date Started

---
+++ JS/20-10-2024/app.js
@@ -0,0 +1,86 @@
+// var randomNum = Math.floor(Math.random() * 6) + 1
+// var randomNum = Math.random() * 4
+// console.log(randomNum.toFixed(0))
+
+
+// var num1 = '35.5'
+// console.log(num1)
+// console.log(typeof(num1)) //string
+// var num2 = parseInt(num1) // 
+// console.log(num2)
+// console.log(typeof(num2))
+// var num3 = parseFloat(num1)
+// console.log(num3)
+// console.log(typeof(num3))
+
+
+
+// var randomValues = 'ABCSWHDAUERM!@#$%^&^%$#32145278175asandjhrwdasuasdmumasas'
+// var randomPass = ''
+
+// Math.abs()  => will return the absoulte number
+
+
+// startsWith , 
+// endsWith
+// includes
+
+
+
+/////// ***************************************  DATE *****************************
+
+
+
+GetTheCurrentDate()
+// var todayDate = new Date().toString()
+
+// var updDate = todayDate.slice(0, 15)
+// console.log(updDate)
+
+
+
+// var days = ['Sun' , 'Mon' , 'Tue' , 'Wed' , 'Thur' , 'Fri' , 'Sat']
+// var today = new Date().getDay()
+// console.log(days[today])
+
+// var month= ["January","February","March","April","May","June","July",
+//     "August","September","October","November","December"];
+
+//     var currentMonth = new Date().getMonth()
+
+//     console.log(month[currentMonth])
+
+
+
+    // var todayDate = new Date()
+    // console.log(todayDate)
+
+
+
+    // //28-Feb-2025
+    // var todayDate  = new Date('28 February , 2025')
+    // console.log(todayDate)
+
+
+    // var firstRamadan  = new Date('09 December , 2000').getTime()
+    // var todayDate = new Date().getTime()
+    // var difference = firstRamadan - todayDate
+    // var dayCalculate = difference / (1000 * 60 * 60 * 24 * 30)   //86400000
+    // console.log(Math.round(dayCalculate))
+
+
+  
+
+    // Normal Function
+
+
+    function GetTheCurrentDate() {
+        var todayDate = new Date()
+        alert(todayDate)
+    }
+
+
+
+
+
+    
