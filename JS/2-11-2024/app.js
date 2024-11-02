6125b00eeb7f87b1129694a35d4ec9ad5fc9d7f9
sualeh-farooq <sualehfarooq65@gmail.com>
2024-11-02 19:44:14 +0500

    Class Code of 2 November 2024
    Topics Covered =>
    Functions
    Passing Them Data
    Switch Cases

---
+++ JS/2-11-2024/app.js
@@ -0,0 +1,79 @@
+// console.log(SumOfTwo(20 , 20));  // Hoisting
+// // console.log(SubOfTwo(100 , 20));
+
+// var org ;
+
+// console.log(org)
+// console.log(SAYLANICLASS)
+
+// // Normal Function
+
+// function SumOfTwo(a,b) {
+//     var result = a + b
+//     return result
+//   // parameters
+// //   var percentage = Math.round((marks / 1000) * 100);
+// //   return percentage;
+// }
+
+// // Arrow Function
+// var SubOfTwo = (a, b) => {
+//   var subtraction = a - b;
+//   return subtraction;
+// };
+
+// var org = 'Saylani'
+// let SAYLANICLASS = 'WEEKEND'
+
+// function Compute(num1, num2, operator) {
+//   var result = "";
+//   if (operator === "+") {
+//     result = num1 + num2;
+//   } else if (operator === "-") {
+//     result = num1 - num2;
+//   } else if (operator === "*") {
+//     result = num1 * num2;
+//   } else if (operator === "/") {
+//     result = num1 / num2;
+//   } else {
+//     result = "Please enter Any Valid Operator !"
+//   }
+//   return result
+// }
+
+// var num1 = +prompt('Enter number 1')
+// var num2 = +prompt('Enter number 2')
+// var operator = prompt('Enter your Operator')
+
+// console.log(Compute(num1 , num2 , operator))
+
+
+// function SumOfTwo () {
+//     var output = 200
+// console.log(output)
+
+// }
+
+
+// SumOfTwo()
+
+
+
+
+
+var day = 'Wed'
+
+switch(day) {
+    case 'Mon':  // if(day === 'Mon')
+        alert('Today is First Day of Week');
+        break;
+    case  'Tue':
+        alert('Today is Second Day of Week');
+        break;
+    default :
+        alert('Today is Funday');
+
+}
+
+
+
