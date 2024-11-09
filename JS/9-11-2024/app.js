c1c6bcdd776775f9b031c4715b2963143e7f01d8
sualeh-farooq <sualehfarooq65@gmail.com>
2024-11-09 19:54:04 +0500

    Class Code of 9-11-2024
    DOM ,
    Key Down Events
    Elements Change from JS

---
+++ JS/9-11-2024/app.js
@@ -0,0 +1,42 @@
+
+
+function clickFunc(){
+    alert('Hello World !')
+}
+
+
+var a = 5
+
+a = 10
+
+function changeCar(){
+event.target.src = './car-run.gif'
+}
+
+function stopCAR() {
+event.target.src = './car-stand.png'
+
+}
+
+
+function checkDiv(){
+    console.log(event)
+}
+
+
+function turnOn() {
+    document.getElementById('bulb').src = './bulb-on.png'
+}
+
+function turnOff() {
+    document.getElementById('bulb').src = './bulb-off.png'
+}
+
+
+function submitForm() {
+    alert('Form Submitted')
+}
+
+function checkCode(){
+  console.log(event.keyCode)
+}
---
+++ JS/9-11-2024/game.js
@@ -0,0 +1,45 @@
+
+
+function playerOneKeys() {
+    Swal.fire("Arrow Keys for Moving and Press D For Fight");
+
+}
+
+function playerTwoKeys() {
+
+}
+
+
+
+var leftAmon = 5
+
+
+window.onkeydown = function() {
+    
+    console.log(event.keyCode)
+    console.log(event.key)
+    if(event.keyCode === 39) {
+        leftAmon += 15
+        document.getElementById('amon').style.left = leftAmon + "px"
+    }
+
+
+    if(event.keyCode === 37) {
+        leftAmon -= 15
+        document.getElementById('amon').style.left = leftAmon + "px"
+    }
+    if(event.keyCode === 68) {
+
+        document.getElementById('amon').classList.add('amon-fight')
+        document.getElementById('amon').src = './amon-fight.gif'
+        setTimeout(() => {
+            document.getElementById('amon').src = './amon-stand.gif'
+        document.getElementById('amon').classList.remove('amon-fight')
+
+        }, 2000);
+    }
+
+
+}
+
+
