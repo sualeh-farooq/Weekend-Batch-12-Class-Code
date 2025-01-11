e7d1f1a6b2318b02f6eb0f810f6887e172172a45
sualeh-farooq <sualehfarooq65@gmail.com>
2025-01-11 19:28:05 +0500

    Class Code of 11-1-2024 , Supabase Started
    Started the Supabase , Setup Supabase Project , and Done Email Authentication

--- JS/29-12-2024/app.js
+++ JS/29-12-2024/app.js
@@ -3,27 +3,18 @@
 //     age: 24
 // }
 
-
 // const {myName} = obj
 
-
-// console.log(myName) 
-
-
+// console.log(myName)
 
 // const fruits = ['Apple' , 'Banana' , 'Mango']
 // const [Apple , ...remainingFruits] = fruits
 // console.log(Apple)
 // console.log(remainingFruits)
 
+// Ternary Operator
 
-// Ternary Operator 
-
-// let userInp = document.getElementById('userNum') 
-
-
-
-
+// let userInp = document.getElementById('userNum')
 
 //     let result = ''
 
@@ -33,12 +24,10 @@
 //     result = 'Less Than 20'
 //  }
 
-
 //  console.log(result)
 
 // let number = +prompt('Enter number')
 // let result = number > 20 && number < 50 ?  'Greater'  :  number < 10 ? 'Less than' : 'Less'
-
 
 // async function fetchTodos() {
 //     try {
@@ -48,13 +37,11 @@
 
 //         showAllTodos(responseJson.todos)
 //     } catch (error) {
-//         console.log(error)        
+//         console.log(error)
 //     }
 // }
 
 // fetchTodos()
-
-
 
 // function filterCompletedTodos(todosData) {
 
@@ -63,11 +50,7 @@
 //     // console.log(completedTodos)
 // }
 
-
-
-
-
-// function showAllTodos(todosData) {   
+// function showAllTodos(todosData) {
 //     let todosList = document.getElementById('todos')
 
 //    todosData.map((inp) => {
@@ -75,27 +58,77 @@
 //         return todosList.innerHTML += `<li> ${inp.todo}  </li>`
 //     }
 //    })
-    // todosData.filter((data)=>data.completed).map((val)=>{
-    //     return todosList.innerHTML += `<li> ${val.todo}  </li>`
-    // })
-    // todosData.map((val , index )=>{
-    //     if(!val.completed) {
-    //         return todosList.innerHTML += `<li>  ${val.todo} and the <b> Index number is ${index} </b> </li>`
-    //     }
-    // })
+// todosData.filter((data)=>data.completed).map((val)=>{
+//     return todosList.innerHTML += `<li> ${val.todo}  </li>`
+// })
+// todosData.map((val , index )=>{
+//     if(!val.completed) {
+//         return todosList.innerHTML += `<li>  ${val.todo} and the <b> Index number is ${index} </b> </li>`
+//     }
+// })
 // }
 
+// function showAllData(...params) {
+//     console.log(params)
+//     const [age ,  todo  , ...rest ] = params
+//     const {completed} = todo
+//     console.log(completed)
+// }
 
+// showAllData(20 ,  {name: 'John' , completed: true}  , 'Ali'  , true)
 
+// const arr =  [{name: "Aatir", email: "aatir@gmail.com"},{name: "Ali", email: "ali@gmail.com"}]
 
-function showAllData(...params) {
-    console.log(params)
-    const [age ,  todo  , ...rest ] = params
-    const {completed} = todo
-    console.log(completed)
+// "2" == 2 //true
+// arr.map((item)=> {
+// console.log(item.name)
+// })
+
+// const arr = [1, 2, 3, 4, 5]
+// const valueToCompare = 2;
+
+// const result = arr.filter(num => num % valueToCompare)
+
+// const arr = [1, 2, 3, 4, 5]
+// const valueToCompare = 2;
+
+// const result = arr.find(num => num == valueToCompare)
+
+// console.log(result)
+
+// const fetchData = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("resolve"),5000)
+//     // reject(("rejected"))
+// })
+
+// fetchData.then((data) => {
+//     console.log("Data fetched")
+// })
+// .catch((error) => {
+//     console.error(error)
+// })
+
+// try {
+//   const result = 10 / 0;
+//   console.log(result)
+// } catch(error) {
+//     console.error(error)
+//     // console.log("An error occured")
+// } finally {
+//     alert("Operation was run successfully.")
+// }
+
+// const a = 10;
+// console.log(a)
+const arr = [[{names: [1,2,3,]}]]
+function abc(a) {
+    return function (number) {
+        
+        return number * a
+    }
+
 }
 
+const test = abc(10);
 
-
-showAllData(20 ,  {name: 'John' , completed: true}  , 'Ali'  , true) 
- 
+console.log(test(2))
---
+++ Supabase/11-1-2025/auth.js
@@ -0,0 +1,84 @@
+let signupEmail = document.getElementById('signup_email')
+let signupPass = document.getElementById('signup_password')
+let signupBtn = document.getElementById('signup-btn')
+let signupBtnLoader = document.getElementById('loading_btn_spinner')
+
+let loginBtn = document.getElementById('login_btn')
+let loginEmail = document.getElementById('login_email')
+let loginPass = document.getElementById('login_password')
+
+let sessionBtn = document.getElementById('session_btn')
+
+let logoutBtn = document.getElementById('logout_btn')
+
+async function signup() {
+  try {
+    signupBtnLoader.style.display = 'block'
+    const { data, error } = await supabase.auth.signUp({
+        email: signupEmail.value,
+        password: signupPass.value,
+      })
+
+    if(error) throw error 
+    if(data) {
+        alert('Please Check your email for confirmation')
+    }
+    return data
+  } catch (error) {
+    console.log(error)
+  } finally {
+    signupBtnLoader.style.display = 'none'
+  }
+      
+}
+
+async function login() {
+    try {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: loginEmail.value,
+            password: loginPass.value,
+          })
+
+          if(error) throw error
+          if(data) {
+            console.log(data)
+            alert('Sign in Succesfull')
+          }
+          return data
+    } catch (error) {
+            console.log(error)
+
+            alert(error.message)
+    }
+}
+
+
+
+async function checkSession(){
+    try {
+        const { data, error } = await supabase.auth.getSession()
+        if(data) {
+            console.log(data)
+        } 
+        if(error) throw error
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+async function logout() {
+    try {
+        const { error } = await supabase.auth.signOut()
+        if(error) throw error
+    } catch (error) {
+    console.log(error)        
+    }
+}
+
+
+sessionBtn.addEventListener('click' , checkSession)
+loginBtn.addEventListener('click' , login)
+
+
+logoutBtn.addEventListener('click' , logout)
